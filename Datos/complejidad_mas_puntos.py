8b31cb27a436c85e8e041faa2be105bf403427ce
Diego Torres <diegotg2000@hotmail.com>
2020-02-19 17:49:58 -0500

    se suben datos y analisis

--- Datos/complejidad_mas_puntos.py
+++ Datos/complejidad_mas_puntos.py
@@ -19,13 +19,17 @@ def complejidad(P):
     U=np.ones(len(P))/len(P)
     return D(P,U)*H(P)/Dstar(n)
 
-NOMBRES=['random_1.0.BSKIndex','random_1.5.BSKIndex','random_2.0.BSKIndex','random_2.5.BSKIndex','random_3.0.BSKIndex','random_3.5.BSKIndex','random_4.0.BSKIndex','random_4.5.BSKIndex','random_5.0.BSKIndex','norandom_1.0.BSKIndex','norandom_1.5.BSKIndex','norandom_2.0.BSKIndex','norandom_2.5.BSKIndex','norandom_3.0.BSKIndex','norandom_3.5.BSKIndex','norandom_4.0.BSKIndex','norandom_4.5.BSKIndex','norandom_5.0.BSKIndex']
-BETAS=[1.0,1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0]
-
-Srandom=np.zeros(9)
-Snorandom=np.zeros(9)
-Crandom=np.zeros(9)
-Cnorandom=np.zeros(9)
+NOMBRES=[]
+BETAS=np.arange(10,51)/10
+for i in range(len(BETAS)):
+    NOMBRES.append('random'+str(BETAS[i])+'.BSKIndex')
+for i in range(len(BETAS)):
+    NOMBRES.append('norandom'+str(BETAS[i])+'.BSKIndex')
+    
+Srandom=np.zeros(len(BETAS))
+Snorandom=np.zeros(len(BETAS))
+Crandom=np.zeros(len(BETAS))
+Cnorandom=np.zeros(len(BETAS))
 
 for i in range(len(NOMBRES)):
     A=np.loadtxt(NOMBRES[i], usecols=0)
@@ -38,7 +42,7 @@ for i in range(len(NOMBRES)):
     
     R=y
     
-    if i <=8:
+    if i <= (len(BETAS)-1):
         puntos=np.loadtxt('random.dat',usecols=0)
         total=len(puntos)
         nonzero=len(nodos)
@@ -54,8 +58,8 @@ for i in range(len(NOMBRES)):
         p0=np.array([(total-nonzero)])
         P=np.concatenate((p0,R),axis=0)
         P=P/total
-        Snorandom[i-9]=entropia(P)
-        Cnorandom[i-9]=complejidad(P)
+        Snorandom[i-len(BETAS)]=entropia(P)
+        Cnorandom[i-len(BETAS)]=complejidad(P)
             
     
 plt.figure(figsize=(5,4))
@@ -64,7 +68,7 @@ plt.plot(BETAS, Snorandom, label='NoRandom',marker='o')
 plt.xlabel('\u03b2')
 plt.ylabel('S')
 plt.legend()
-plt.savefig('SvBeta.png')
+plt.savefig('SvBeta_masdatos.png')
 
 plt.figure(figsize=(5,4))
 plt.plot(BETAS, Crandom, label='Random',marker='o')
@@ -72,7 +76,7 @@ plt.plot(BETAS, Cnorandom, label='NoRandom',marker='o')
 plt.xlabel('\u03b2')
 plt.ylabel('C')
 plt.legend()
-plt.savefig('CvBeta.png')
+plt.savefig('CvBeta_masdatos.png')
 
 plt.figure(figsize=(5,4))
 plt.plot(Srandom, Crandom, label='Random',marker='o')
@@ -80,7 +84,7 @@ plt.plot(Snorandom, Cnorandom, label='NoRandom',marker='o')
 plt.xlabel('S')
 plt.ylabel('C')
 plt.legend()
-plt.savefig('SvC.png')
+plt.savefig('SvC_masdatos.png')
     
     
     
